6f5accf1e9a27464e720a68c800c9c92384115d5
boucaDOTdev <101053274+boucaDOTdev@users.noreply.github.com>
2022-03-22 21:03:38 +0000

    Understanding Classes

--- section-2-JavaScript-refresher/main.js
+++ section-2-JavaScript-refresher/main.js
@@ -22,3 +22,29 @@ const multiply = (number) => {
   return number * 2
 }
 console.log(multiply(2))
+
+//Classes
+class Human {
+  constructor() {
+    this.gender = 'male'
+  }
+
+  printGender() {
+    console.log(this.gender)
+  }
+}
+
+class Person extends Human {
+  constructor() {
+    super()
+    this.name = 'Alex'
+  }
+
+  printMyName() {
+    console.log(this.name)
+  }
+}
+
+const person = new Person()
+person.printMyName()
+person.printGender()
