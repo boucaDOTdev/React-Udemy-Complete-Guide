250c97b5363c592f94a66d136a62baa8f50cfb4a
boucaDOTdev <101053274+boucaDOTdev@users.noreply.github.com>
2022-03-23 16:55:58 +0000

    Update ExpenseItem

--- starting-react/src/components/ExpenseItem.js
+++ starting-react/src/components/ExpenseItem.js
@@ -1,4 +1,11 @@
 function ExpenseItem() {
-  return <h2>Expence Item!</h2>
+  return (
+    <div>
+      <div>March 28th 2022</div>
+      <div>
+        <h2>Car Insurance</h2> <div>$249.67</div>
+      </div>
+    </div>
+  )
 }
 export default ExpenseItem
