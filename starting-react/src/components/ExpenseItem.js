98024243a060517be713716f5e05dcaed7c5cb72
boucaDOTdev <101053274+boucaDOTdev@users.noreply.github.com>
2022-03-23 17:04:26 +0000

    adding css

--- starting-react/src/components/ExpenseItem.js
+++ starting-react/src/components/ExpenseItem.js
@@ -1,9 +1,12 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>March 28th 2022</div>
-      <div>
-        <h2>Car Insurance</h2> <div>$249.67</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$249.67</div>
       </div>
     </div>
   )
