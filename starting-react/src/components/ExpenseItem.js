ab46808b1e8c10bdb45a70ddf9f9c815f64f5f36
boucaDOTdev <101053274+boucaDOTdev@users.noreply.github.com>
2022-03-24 16:33:47 +0000

    Splinting date into 3 variables

--- starting-react/src/components/ExpenseItem.js
+++ starting-react/src/components/ExpenseItem.js
@@ -1,13 +1,19 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
-  const expenseDate = props.date
+  const month = props.date.toLocaleString('en-US', { month: 'long' })
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' })
+  const year = props.date.getFullYear()
   const expenseTitle = props.title
   const expenseAmount = props.amount
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">$ {expenseAmount}</div>
